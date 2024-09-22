c650a7c9136ca1cd99da67ecd62170c688024e13
prashil107 <prashilsingh107@gmail.com>
2024-09-22 12:48:29 +0530

    Implementation of Node.js

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+//var generateName = require("sillyname");
+
+//import generateName from "sillyname";
+//const sillyname = generateName();
+
+//console.log("My name is " + sillyname);
+
+import { randomSuperhero } from 'superheroes';
+const name = randomSuperhero();
+
+console.log("My name is "+ name);
